a4803f66f6427c177d78606b03657c1d6bdd1d3a
erencimen <erencimen@users.noreply.github.com>
2019-12-26 16:41:47 +0300

    Update main.c

--- STM32F407xx_spi_driver/Src/main.c
+++ STM32F407xx_spi_driver/Src/main.c
@@ -380,7 +380,9 @@ int main(void)
 			//Read the Ardunio ID recieved
 			SPI_ReceiveData(SPI2, &Ardunio_ID, 1);
 		}
-		
+
+		while (SPI_GetFlagStatus(SPI2, SPI_BUSY_FLAG)) ;
+
 		SPI_PeripheralControl(SPI2, DISABLE);
 
 	}
